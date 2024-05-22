782b46ba333202b39a51d230fae17e192d59faf3
atulanurag28 <atul.anand.ug21@nsut.ac.in>
2024-05-22 20:41:33 +0530

    Finished Backend

---
+++ constants/config.js
@@ -0,0 +1,14 @@
+import cors from 'cors'
+const corsOptions = {
+  origin: [
+    "http://localhost:5176",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
