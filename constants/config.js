9468c68900ad3875d23d269e323019151a1e751b
atulanurag28 <atul.anand.ug21@nsut.ac.in>
2024-05-23 13:01:58 +0530

    xyz again

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,7 @@
 import cors from 'cors'
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
+    "http://localhost:5174",
     "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
